cd63d0d73701d1d174b65eb5dc577bbbf57843a0
Mikael Hornaeus <mikael.hornaeus@gmail.com>
2015-06-04 11:47:31 +0100

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-
-#Required for Heroku    
-config.action_mailer.default_url_options = { host => 'https://mickeapp.herokuapp.com/'}  
+  #Required for Heroku    
+  config.action_mailer.default_url_options = { host => 'https://mickeapp.herokuapp.com/'}  
+  
 end
