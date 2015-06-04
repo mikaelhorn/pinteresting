ffc2aadd5922ea0416103cabd7fcd6306bda4a8d
Mikael Hornaeus <mikael.hornaeus@gmail.com>
2015-06-04 11:41:03 +0100

    changed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,8 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-end
+
 
 
 #Required for Heroku    
 config.action_mailer.default_url_options = { host => 'https://mickeapp.herokuapp.com/'}  
+end
